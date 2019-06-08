4f39d75b78397f48f54df3c59465e38d7560f473
ZinggJM <jean-marc.zingg@bluewin.ch>
2019-06-08 14:58:44 +0200

    pre preliminary Version 1.0.0

---
+++ src/GxEPD2_EPD.cpp
@@ -0,0 +1,267 @@
+// Display Library for SPI e-paper panels from Dalian Good Display and boards from Waveshare.
+// Requires HW SPI and Adafruit_GFX. Caution: these e-papers require 3.3V supply AND data lines!
+//
+// based on Demo Example from Good Display: http://www.e-paper-display.com/download_list/downloadcategoryid=34&isMode=false.html
+//
+// Author: Jean-Marc Zingg
+//
+// Version: see library.properties
+//
+// Library: https://github.com/ZinggJM/GxEPD2
+
+#include "GxEPD2_EPD.h"
+
+#if defined(ESP8266) || defined(ESP32)
+#include <pgmspace.h>
+#else
+#include <avr/pgmspace.h>
+#endif
+
+GxEPD2_EPD::GxEPD2_EPD(int8_t cs, int8_t dc, int8_t rst, int8_t busy, int8_t busy_level, uint32_t busy_timeout,
+                       uint16_t w, uint16_t h, GxEPD2::Panel p, bool c, bool pu, bool fpu) :
+  WIDTH(w), HEIGHT(h), panel(p), hasColor(c), hasPartialUpdate(pu), hasFastPartialUpdate(fpu),
+  _cs(cs), _dc(dc), _rst(rst), _busy(busy), _busy_level(busy_level), _busy_timeout(busy_timeout), _diag_enabled(false),
+  _spi_settings(4000000, MSBFIRST, SPI_MODE0)
+{
+  _initial_write = true;
+  _initial_refresh = true;
+  _power_is_on = false;
+  _using_partial_mode = false;
+  _hibernating = false;
+}
+
+void GxEPD2_EPD::init(uint32_t serial_diag_bitrate)
+{
+  init(serial_diag_bitrate, true, false);
+}
+
+void GxEPD2_EPD::init(uint32_t serial_diag_bitrate, bool initial, bool pulldown_rst_mode)
+{
+  _initial_write = initial;
+  _initial_refresh = initial;
+  _pulldown_rst_mode = pulldown_rst_mode;
+  _power_is_on = false;
+  _using_partial_mode = false;
+  _hibernating = false;
+  if (serial_diag_bitrate > 0)
+  {
+    Serial.begin(serial_diag_bitrate);
+    _diag_enabled = true;
+  }
+  if (_cs >= 0)
+  {
+    digitalWrite(_cs, HIGH);
+    pinMode(_cs, OUTPUT);
+  }
+  if (_dc >= 0)
+  {
+    digitalWrite(_dc, HIGH);
+    pinMode(_dc, OUTPUT);
+  }
+  _reset();
+  if (_busy >= 0)
+  {
+    pinMode(_busy, INPUT);
+  }
+  SPI.begin();
+  _sram_init();
+}
+
+void GxEPD2_EPD::_reset()
+{
+  if (_rst >= 0)
+  {
+    if (_pulldown_rst_mode)
+    {
+      digitalWrite(_rst, LOW);
+      pinMode(_rst, OUTPUT);
+      delay(20);
+      pinMode(_rst, INPUT_PULLUP);
+      delay(200);
+    }
+    else
+    {
+      digitalWrite(_rst, HIGH);
+      pinMode(_rst, OUTPUT);
+      delay(20);
+      digitalWrite(_rst, LOW);
+      delay(20);
+      digitalWrite(_rst, HIGH);
+      delay(200);
+    }
+    _hibernating = false;
+  }
+}
+
+void GxEPD2_EPD::_waitWhileBusy(const char* comment, uint16_t busy_time)
+{
+  if (_busy >= 0)
+  {
+    delay(1); // add some margin to become active
+    unsigned long start = micros();
+    while (1)
+    {
+      if (digitalRead(_busy) != _busy_level) break;
+      delay(1);
+      if (micros() - start > _busy_timeout)
+      {
+        Serial.println("Busy Timeout!");
+        break;
+      }
+    }
+    if (comment)
+    {
+#if !defined(DISABLE_DIAGNOSTIC_OUTPUT)
+      if (_diag_enabled)
+      {
+        unsigned long elapsed = micros() - start;
+        Serial.print(comment);
+        Serial.print(" : ");
+        Serial.println(elapsed);
+      }
+#endif
+    }
+    (void) start;
+  }
+  else delay(busy_time);
+}
+
+void GxEPD2_EPD::_writeCommand(uint8_t c)
+{
+  SPI.beginTransaction(_spi_settings);
+  if (_dc >= 0) digitalWrite(_dc, LOW);
+  if (_cs >= 0) digitalWrite(_cs, LOW);
+  SPI.transfer(c);
+  if (_cs >= 0) digitalWrite(_cs, HIGH);
+  if (_dc >= 0) digitalWrite(_dc, HIGH);
+  SPI.endTransaction();
+}
+
+void GxEPD2_EPD::_writeData(uint8_t d)
+{
+  SPI.beginTransaction(_spi_settings);
+  if (_cs >= 0) digitalWrite(_cs, LOW);
+  SPI.transfer(d);
+  if (_cs >= 0) digitalWrite(_cs, HIGH);
+  SPI.endTransaction();
+}
+
+void GxEPD2_EPD::_writeData(const uint8_t* data, uint16_t n)
+{
+  SPI.beginTransaction(_spi_settings);
+  if (_cs >= 0) digitalWrite(_cs, LOW);
+  for (uint16_t i = 0; i < n; i++)
+  {
+    SPI.transfer(*data++);
+  }
+  if (_cs >= 0) digitalWrite(_cs, HIGH);
+  SPI.endTransaction();
+}
+
+void GxEPD2_EPD::_writeDataPGM(const uint8_t* data, uint16_t n)
+{
+  SPI.beginTransaction(_spi_settings);
+  if (_cs >= 0) digitalWrite(_cs, LOW);
+  for (uint16_t i = 0; i < n; i++)
+  {
+    SPI.transfer(pgm_read_byte(&*data++));
+  }
+  if (_cs >= 0) digitalWrite(_cs, HIGH);
+  SPI.endTransaction();
+}
+
+void GxEPD2_EPD::_writeCommandData(const uint8_t* pCommandData, uint8_t datalen)
+{
+  SPI.beginTransaction(_spi_settings);
+  if (_dc >= 0) digitalWrite(_dc, LOW);
+  if (_cs >= 0) digitalWrite(_cs, LOW);
+  SPI.transfer(*pCommandData++);
+  if (_dc >= 0) digitalWrite(_dc, HIGH);
+  for (uint8_t i = 0; i < datalen - 1; i++)  // sub the command
+  {
+    SPI.transfer(*pCommandData++);
+  }
+  if (_cs >= 0) digitalWrite(_cs, HIGH);
+  SPI.endTransaction();
+}
+
+void GxEPD2_EPD::_writeCommandDataPGM(const uint8_t* pCommandData, uint8_t datalen)
+{
+  SPI.beginTransaction(_spi_settings);
+  if (_dc >= 0) digitalWrite(_dc, LOW);
+  if (_cs >= 0) digitalWrite(_cs, LOW);
+  SPI.transfer(pgm_read_byte(&*pCommandData++));
+  if (_dc >= 0) digitalWrite(_dc, HIGH);
+  for (uint8_t i = 0; i < datalen - 1; i++)  // sub the command
+  {
+    SPI.transfer(pgm_read_byte(&*pCommandData++));
+  }
+  if (_cs >= 0) digitalWrite(_cs, HIGH);
+  SPI.endTransaction();
+}
+
+// SRAM support
+#define SPIRAM_CS 5
+#define CMD_WRSR  0x01
+#define CMD_READ  0x03
+#define CMD_WRITE 0x02
+#define BYTE_MODE 0x00
+
+uint32_t GxEPD2_EPD::_sram_break = 2; // avoid NULL!
+
+void GxEPD2_EPD::_sram_init()
+{
+  digitalWrite(SPIRAM_CS, HIGH);
+  pinMode(SPIRAM_CS, OUTPUT);
+  SPI.beginTransaction(_spi_settings);
+  digitalWrite(SPIRAM_CS, LOW);
+  SPI.transfer(CMD_WRSR);
+  SPI.transfer(BYTE_MODE);
+  digitalWrite(SPIRAM_CS, HIGH);
+  SPI.endTransaction();
+}
+
+uint32_t GxEPD2_EPD::_sram_alloc(uint32_t amount)
+{
+  uint32_t rv = _sram_break;
+  if (_sram_break + amount < 128ul * 1024ul)
+  {
+    _sram_break += amount;
+  }
+  else
+  {
+    // overwrite is best option (should never occur)
+    rv = 2; // avoid NULL!
+    _sram_break = amount + 2;
+  }
+  return rv;
+}
+
+uint8_t GxEPD2_EPD::_readByte(uint32_t addr)
+{
+  uint32_t addrvalue = uint32_t(addr);
+  SPI.beginTransaction(_spi_settings);
+  digitalWrite(SPIRAM_CS, LOW);
+  SPI.transfer(CMD_READ);
+  SPI.transfer(uint8_t(addrvalue >> 16));
+  SPI.transfer(uint8_t(addrvalue >> 8));
+  SPI.transfer(uint8_t(addrvalue));
+  uint8_t rv = SPI.transfer(0);
+  digitalWrite(SPIRAM_CS, HIGH);
+  SPI.endTransaction();
+  return rv;
+}
+
+void GxEPD2_EPD::_writeByte(uint32_t addr, uint8_t data)
+{
+  uint32_t addrvalue = uint32_t(addr);
+  SPI.beginTransaction(_spi_settings);
+  digitalWrite(SPIRAM_CS, LOW);
+  SPI.transfer(CMD_WRITE);
+  SPI.transfer(uint8_t(addrvalue >> 16));
+  SPI.transfer(uint8_t(addrvalue >> 8));
+  SPI.transfer(uint8_t(addrvalue));
+  SPI.transfer(data);
+  digitalWrite(SPIRAM_CS, HIGH);
+  SPI.endTransaction();
+}
